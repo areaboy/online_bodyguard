3d195fb31192e59343a1cd374a8c81b6b6bc739d
Esedo Fredrick <50068759+areaboy@users.noreply.github.com>
2023-06-11 13:43:27 +0100

    Update settings.php

--- online_bodyguard/settings.php
+++ online_bodyguard/settings.php
@@ -1,10 +1,10 @@
-﻿<?php
+<?php
 error_reporting(0);
 // Set Pangea url-intel Access Token
 
-$url_intel_accesstoken =  "pts_6yaueljc4fmxkeguneprfz4zxrbx25wc";
+$url_intel_accesstoken =  "";
 
 // Set User-Intel Access Token
-$user_intel_accesstoken =  "pts_ucdydv3iog7d66vn4vgqtssnb6hzddzq";
+$user_intel_accesstoken =  "";
 
 ?>
